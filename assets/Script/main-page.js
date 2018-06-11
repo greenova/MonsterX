2fc3a89bc413b9b25ab28b2328b0c92fffc204f7
unknown <jogreen1998@yahoo.co.nz>
2018-06-11 20:48:06 +1200

    Release build

--- assets/Script/main-page.js
+++ assets/Script/main-page.js
@@ -35,6 +35,7 @@ cc.Class({
 
     start () {
 
+
     },
 
     // update (dt) {},
---
+++ library/imports/19/193fb9dc-a673-4441-969d-1360a45e93b5.js
@@ -0,0 +1,53 @@
+"use strict";
+cc._RF.push(module, '193fbncpnNEQZadE2CkXpO1', 'explor');
+// Script/explor.js
+
+'use strict';
+
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    onLoad: function onLoad() {
+
+        var realUrl = cc.url.raw('background/9.png');
+        var texture = cc.textureCache.addImage(realUrl);
+
+        this.getComponent(cc.Sprite).spriteFrame.setTexture(texture);
+    }
+
+    // update (dt) {},
+});
+
+cc._RF.pop();
